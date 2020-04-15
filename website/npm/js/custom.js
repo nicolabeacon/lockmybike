16111cda6be5a1af9c89ce18471fbf5a46aff603
Ricardo Villamil <ricardovj1@gmail.com>
2020-04-14 21:22:09 -0400

    Fix locate

--- website/npm/js/custom.js
+++ website/npm/js/custom.js
@@ -1,92 +1,96 @@
 // custom JS code
-var mymap;
-var lyrOSM;
-var lyrLocate;
-var nearSpotsIndex;
-var sidebar;
-var leafletSearch;
+var mymap
+var lyrOSM
+var lyrLocate
+var nearSpotsIndex
+var sidebar
+var leafletSearch
 
-
-$(document).ready(function() {
-  mymap = L.map('map').setView([42.358918, -71.063828], 13);
+$(document).ready(function () {
+  mymap = L.map('map').setView([42.358918, -71.063828], 13)
   lyrOSM = L.tileLayer(
-    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+    {
+      attribution:
+        'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
       id: 'mapbox/streets-v11',
-      accessToken: 'pk.eyJ1IjoibmljZW9sYTg4IiwiYSI6ImNrNmZpMTcwczF6Z24zbm4zeXhnbGZocngifQ.VBUYpku7fhmot35fpOp8fQ'
+      accessToken:
+        'pk.eyJ1IjoibmljZW9sYTg4IiwiYSI6ImNrNmZpMTcwczF6Z24zbm4zeXhnbGZocngifQ.VBUYpku7fhmot35fpOp8fQ',
     }
-  ).addTo(mymap);
-
+  ).addTo(mymap)
 
   sidebar = L.control.sidebar('sidebar', {
-    position: 'left'
-  });
+    position: 'left',
+  })
 
-  mymap.addControl(sidebar);
+  mymap.addControl(sidebar)
 
   // add a custom button to toggle sidebar
 
-  L.easyButton('fa-arrows-alt-h', function() {
-    if (sidebar.isVisible()) {
-      sidebar.hide();
-    } else {
-      sidebar.show();
-    }
-  }, 'look for stations near you').addTo(mymap);
+  L.easyButton(
+    'fa-arrows-alt-h',
+    function () {
+      if (sidebar.isVisible()) {
+        sidebar.hide()
+      } else {
+        sidebar.show()
+      }
+    },
+    'look for stations near you'
+  ).addTo(mymap)
 
   // find your location adding a circle if button "locate" is pressed; also, handle location failure
 
-  
-    function nearbyMarkers(e) {
-            nearSpotsIndex = leafletKnn(lyrLocate);
-            var nearestResult = nearSpotsIndex.nearest(e.latlng, 10);
-            return nearestResult.latlng === L.Marker.latlng;
-          }
+  $('#btnLocate').click(function () {
+    mymap.locate()
+  })
 
-  
-   $('#btnLocate').click(function() {
-    mymap.locate();
-  });
+  mymap.on('locationfound', function (e) {
+    console.log(e)
 
-  mymap.on('locationfound', function(e) {
-    console.log(e);
-        $.getJSON('../bikedata.geojson', function(data) {
+    $.getJSON('../bikedata.geojson', function (data) {
+      const nearestResults = leafletKnn(L.geoJson(data)).nearest(e.latlng, 10)
+
       //add custom icons
       var rackIcon = L.icon({
         iconUrl: '../images/bikeRing.png',
-        iconSize: [60, 33.6]
-      });
-      
-      // add GeoJSON layer to the map once the file is loaded
-       lyrLocate = L.geoJson(data, {
-        pointToLayer: function(feature, latlng) {
+        iconSize: [60, 33.6],
+      })
+
+      L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
           return L.marker(latlng, {
-            icon: rackIcon
-          });
+            icon: rackIcon,
+          })
         },
-        onEachFeature: function(feature, layer) {
+        onEachFeature: function (feature, layer) {
           layer.bindPopup(
             'Address:' + '&nbsp' + feature.properties.rack_street_address
-          );
+          )
         },
-        filter: nearbyMarkers
-      }).addTo(mymap);
-    });
-  });
+        filter: nearbyMarkers,
+      }).addTo(mymap)
 
-  mymap.on('locationerror', function(e) {
-    console.log(e);
-    alert('the location was not found');
-  });
-  
-  
-  
+      function nearbyMarkers(feature) {
+        var found = false
+        for (let i = 0; i < nearestResults.length; i++) {
+          const nearestResult = nearestResults[i]
 
+          if (found) return true
 
+          found =
+            nearestResult.lat == feature.properties.lat &&
+            nearestResult.lon == feature.properties.long
+        }
+      }
+    })
+  })
 
-
-
+  mymap.on('locationerror', function (e) {
+    console.log(e)
+    alert('the location was not found')
+  })
 
   //////////////////////////////////////////
 
@@ -95,10 +99,8 @@ $(document).ready(function() {
   leafletSearch = L.Control.openCageSearch({
     key: 'd7f2af1decc940898adba6b32b6a2e7d',
     limit: 10,
-    position: 'topleft'
-  }).addTo(mymap);
-
-});
-
+    position: 'topleft',
+  }).addTo(mymap)
+})
 
 // end of document ready function
